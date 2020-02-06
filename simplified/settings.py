e35dcda8439e45836b3b24d7ae30d005116c1917
andrey <enron07@gmail.com>
2020-02-06 19:39:34 +0300

    Create settings.py

---
+++ simplified/settings.py
@@ -0,0 +1,9 @@
+COMISSION = 0.0 #for opening and closing position
+SLIPAGE = 0.0 # deviation from the price at the trading screen
+REPLAY_PROB = 0.2 # probabiliy to sample day for replay
+TRAINING = True # training mode
+REPLAY_BUFFER_SIZE = 1000
+LAYERS = [14,8,5,1] #initial layer estimations*levels +5 (actions)
+DUELING = True
+DOUBLE = True
+GAMMA = 0.999
